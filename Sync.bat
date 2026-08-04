813f681641c6993a749507a8ed91e5c7987aac81
Anish T <tamilneram247@gmail.com>
2026-08-04 12:56:32 +0530

    updated

---
+++ Sync.bat
@@ -0,0 +1,13 @@
+@echo off
+
+git clone https://github.com/247i/Git
+git clone https://github.com/247i/GitExt
+git clone https://github.com/247i/PuTTY
+git clone https://github.com/247i/WinMerge
+git clone https://github.com/247i/WinSCP
+
+:: Get the VSCode portable into vscode folder manually
+
+:: Get the VSCodium portable into vscodium folder manually
+
+pause
