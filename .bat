bd656d0eac205d60f4570d8e5cf652d6e126a0f0
Anish T <tamilneram247@gmail.com>
2026-08-04 12:50:43 +0530

    Update .bat

--- .bat
+++ .bat
@@ -1,33 +1,11 @@
 @echo off
 
 git clone https://github.com/247i/Git 
-pause
-set "GitExt_REPO=https://github.com/247i/GitExt"
-set "GitExt_FOLDER="
-:: Clone the repository into the specified subfolder
-git clone %GitExt_REPO% %GitExt_FOLDER%
-
-set "PuTTY_REPO=https://github.com/247i/GitHubDesktop.git"
-set "PuTTY_FOLDER="
-:: Clone the repository into the specified subfolder
-git clone %PuTTY_REPO% %PuTTY_FOLDER%
-
-set "PuTTY_REPO=https://github.com/247i/PuTTY.git"
-set "PuTTY_FOLDER="
-:: Clone the repository into the specified subfolder
-git clone %PuTTY_REPO% %PuTTY_FOLDER%
-
-
-set "WinMerge_REPO=https://github.com/247i/WinMerge.git"
-set "WinMerge_FOLDER="
-:: Clone the repository into the specified subfolder
-git clone %WinMerge_REPO% %WinMerge_FOLDER%
-
-
-set "WinSCP_REPO=https://github.com/247i/WinSCP.git"
-set "WinSCP_FOLDER="
-:: Clone the repository into the specified subfolder
-git clone %WinSCP_REPO% %WinSCP_FOLDER%
+git clone https://github.com/247i/GitExt 
+git clone https://github.com/247i/GitHubDesktop.git 
+git clone https://github.com/247i/PuTTY.git 
+git clone https://github.com/247i/WinMerge.git 
+git clone https://github.com/247i/WinSCP.git 
 
 :: Get the VSCode portable into vscode folder manually
 
