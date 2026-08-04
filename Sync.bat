5c77006957b7b8e70967873b2dc0911959ea0e19
Anish T <tamilneram247@gmail.com>
2026-08-04 23:29:09 +0530

    Update Sync.bat

--- Sync.bat
+++ Sync.bat
@@ -1,17 +1,17 @@
 @echo off
-git clone https://github.com/247i/7z
-git clone https://github.com/247i/Azhagi
-git clone https://github.com/247i/Everything
-git clone https://github.com/247i/Git
-git clone https://github.com/247i/GitExt
-git clone https://github.com/247i/IniyaTamil
-git clone https://github.com/247i/NotePadPP
-git clone https://github.com/247i/PuTTY
-git clone https://github.com/247i/WinMerge
-git clone https://github.com/247i/WinSCP
-git clone https://github.com/247i/SourceGit
-git clone https://github.com/247i/SumatraPDF
-git clone https://github.com/247i/Thonny
+git clone --depth 1 https://github.com/247i/7z
+git clone --depth 1 https://github.com/247i/Azhagi
+git clone --depth 1 https://github.com/247i/Everything
+git clone --depth 1 https://github.com/247i/Git
+git clone --depth 1 https://github.com/247i/GitExt
+git clone --depth 1 https://github.com/247i/IniyaTamil
+git clone --depth 1 https://github.com/247i/NotePadPP
+git clone --depth 1 https://github.com/247i/PuTTY
+git clone --depth 1 https://github.com/247i/WinMerge
+git clone --depth 1 https://github.com/247i/WinSCP
+git clone --depth 1 https://github.com/247i/SourceGit
+git clone --depth 1 https://github.com/247i/SumatraPDF
+git clone --depth 1 https://github.com/247i/Thonny
 
 :: Get the VSCode portable into vscode folder manually
 
