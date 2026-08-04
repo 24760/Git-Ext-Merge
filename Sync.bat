d3eec74d511972c7ef5a30957aad3dae52052894
Anish T <tamilneram247@gmail.com>
2026-08-04 13:17:43 +0530

    Update Sync.bat

--- Sync.bat
+++ Sync.bat
@@ -5,6 +5,7 @@ git clone https://github.com/247i/GitExt
 git clone https://github.com/247i/PuTTY
 git clone https://github.com/247i/WinMerge
 git clone https://github.com/247i/WinSCP
+git clone https://github.com/247i/SourceGit
 
 :: Get the VSCode portable into vscode folder manually
 
