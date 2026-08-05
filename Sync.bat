01c3a8ba2eeafdda5412ac13f33fdf82fcab900b
Anish T <tamilneram247@gmail.com>
2026-08-05 13:43:20 +0530

    megit

--- Sync.bat
+++ Sync.bat
@@ -5,6 +5,7 @@ git clone --depth 1 https://github.com/247i/Everything
 git clone --depth 1 https://github.com/247i/Git
 git clone --depth 1 https://github.com/247i/GitExt
 git clone --depth 1 https://github.com/247i/IniyaTamil
+git clone --depth 1 https://github.com/247i/MeGit
 git clone --depth 1 https://github.com/247i/NotePadPP
 git clone --depth 1 https://github.com/247i/PuTTY
 git clone --depth 1 https://github.com/247i/SourceGit
@@ -16,6 +17,6 @@ git clone --depth 1 https://github.com/247i/WinSCP
 :: Get the VSCode portable into vscode folder manually
 
 :: Get the VSCodium portable into vscodium folder manually
-
+:: RelaGit
 :: Get the ScreenToGif portable into ScreenToGif folder manually
 pause
