47ef5880d80ec5bc15f0307dbc2950f870597963
Anish T <tamilneram247@gmail.com>
2026-08-05 14:17:34 +0530

    geany

--- Sync.bat
+++ Sync.bat
@@ -2,6 +2,7 @@
 git clone --depth 1 https://github.com/247i/7z
 git clone --depth 1 https://github.com/247i/Azhagi
 git clone --depth 1 https://github.com/247i/Everything
+git clone --depth 1 https://github.com/247i/Geany
 git clone --depth 1 https://github.com/247i/Git
 git clone --depth 1 https://github.com/247i/GitExt
 git clone --depth 1 https://github.com/247i/IniyaTamil
