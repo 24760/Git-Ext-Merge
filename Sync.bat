b0bb00eed8b60081e7077e2eef28fb20401201ae
தமிழ்நேரம் <tamilneram247@gmail.com>
2026-08-08 11:38:30 +0530

    Update Sync.bat

--- Sync.bat
+++ Sync.bat
@@ -1,5 +1,4 @@
 @echo off
-git clone --depth 1 https://github.com/247i/.net
 git clone --depth 1 https://github.com/247i/.net07x64
 git clone --depth 1 https://github.com/247i/.net07x86
 git clone --depth 1 https://github.com/247i/.net08x64
@@ -10,7 +9,10 @@ git clone --depth 1 https://github.com/247i/.net10x64
 git clone --depth 1 https://github.com/247i/.net10x86
 git clone --depth 1 https://github.com/247i/.net11x64
 git clone --depth 1 https://github.com/247i/.net11x86
-git clone --depth 1 https://github.com/247i/7z
+git clone --depth 1 https://github.com/247i/7zx64
+git clone --depth 1 https://github.com/247i/7zx86
+git clone --depth 1 https://github.com/247i/7zetc
+git clone --depth 1 https://github.com/247i/7zSDK
 git clone --depth 1 https://github.com/247i/Azhagi
 git clone --depth 1 https://github.com/247i/Everything
 git clone --depth 1 https://github.com/247i/FileZilla
