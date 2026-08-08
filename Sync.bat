0ca570ec115f703d278fa679b6fe8eca8cc055f1
தமிழ்நேரம் <tamilneram247@gmail.com>
2026-08-08 07:35:56 +0530

    Update Sync.bat

--- Sync.bat
+++ Sync.bat
@@ -1,4 +1,5 @@
 @echo off
+git clone --depth 1 https://github.com/247i/.net
 git clone --depth 1 https://github.com/247i/7z
 git clone --depth 1 https://github.com/247i/Azhagi
 git clone --depth 1 https://github.com/247i/Everything
@@ -9,7 +10,6 @@ git clone --depth 1 https://github.com/247i/GitExt
 git clone --depth 1 https://github.com/247i/IniyaTamil
 git clone --depth 1 https://github.com/247i/MeGit
 git clone --depth 1 https://github.com/247i/NotePadPP
-git clone --depth 1 https://github.com/247i/progit2
 git clone --depth 1 https://github.com/247i/PuTTY
 git clone --depth 1 https://github.com/247i/SourceGit
 git clone --depth 1 https://github.com/247i/SumatraPDF
