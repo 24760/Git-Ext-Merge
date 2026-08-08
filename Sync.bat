76e4242e87b7ebd2f2adbd3ead868473fb058ab9
Anish T <tamilneram247@gmail.com>
2026-08-08 06:49:35 +0530

    FileZilla

--- Sync.bat
+++ Sync.bat
@@ -2,12 +2,14 @@
 git clone --depth 1 https://github.com/247i/7z
 git clone --depth 1 https://github.com/247i/Azhagi
 git clone --depth 1 https://github.com/247i/Everything
+git clone --depth 1 https://github.com/247i/FileZilla
 git clone --depth 1 https://github.com/247i/Geany
 git clone --depth 1 https://github.com/247i/Git
 git clone --depth 1 https://github.com/247i/GitExt
 git clone --depth 1 https://github.com/247i/IniyaTamil
 git clone --depth 1 https://github.com/247i/MeGit
 git clone --depth 1 https://github.com/247i/NotePadPP
+git clone --depth 1 https://github.com/247i/progit2
 git clone --depth 1 https://github.com/247i/PuTTY
 git clone --depth 1 https://github.com/247i/SourceGit
 git clone --depth 1 https://github.com/247i/SumatraPDF
