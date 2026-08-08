c0dc34b3718fc03c2718711181c221d4241c144b
தமிழ்நேரம் <tamilneram247@gmail.com>
2026-08-08 21:05:48 +0530

    Update Sync.bat

--- Sync.bat
+++ Sync.bat
@@ -1,23 +1,11 @@
 @echo off
 git clone --depth 1 https://github.com/247i/.net07x64
-git clone --depth 1 https://github.com/247i/.net07x86
-git clone --depth 1 https://github.com/247i/.net08x64
-git clone --depth 1 https://github.com/247i/.net08x86
-git clone --depth 1 https://github.com/247i/.net09x64
-git clone --depth 1 https://github.com/247i/.net09x86
-git clone --depth 1 https://github.com/247i/.net10x64
-git clone --depth 1 https://github.com/247i/.net10x86
-git clone --depth 1 https://github.com/247i/.net11x64
-git clone --depth 1 https://github.com/247i/.net11x86
 git clone --depth 1 https://github.com/247i/7zx64
-git clone --depth 1 https://github.com/247i/7zx86
-git clone --depth 1 https://github.com/247i/7zetc
-git clone --depth 1 https://github.com/247i/7zSDK
 git clone --depth 1 https://github.com/247i/Azhagi
 git clone --depth 1 https://github.com/247i/Everything
 git clone --depth 1 https://github.com/247i/FileZilla
 git clone --depth 1 https://github.com/247i/Geany
-git clone --depth 1 https://github.com/247i/Git
+git clone --depth 1 https://github.com/247i/Gitx64
 git clone --depth 1 https://github.com/247i/GitExt
 git clone --depth 1 https://github.com/247i/IniyaTamil
 git clone --depth 1 https://github.com/247i/MeGit
